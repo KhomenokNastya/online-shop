60e5f57fa1d309e9cbb0921b2a1943aebe410482
KhomenokNastya <khomenok.nastya@bk.ru>
2024-02-03 18:40:51 +0300

    Slug converter for product

--- goods/urls.py
+++ goods/urls.py
@@ -5,5 +5,5 @@ app_name = 'goods'
 
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
