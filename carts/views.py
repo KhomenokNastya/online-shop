9c92f2b2b53b64175634ad0da330be89cadad8a2
KhomenokNastya <khomenok.nastya@bk.ru>
2024-02-17 14:17:04 +0300

    Cart remove controller added, commit before ajax

--- carts/views.py
+++ carts/views.py
@@ -26,5 +26,7 @@ def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, product_slug):
-    ...
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META['HTTP_REFERER'])
