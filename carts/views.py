99fe9a1f9dd3bfb4c39a62d73fd81aa3b3645bd4
KhomenokNastya <khomenok.nastya@bk.ru>
2024-02-18 12:49:05 +0300

    Change cart ajax and json response, all ajax done

--- carts/views.py
+++ carts/views.py
@@ -39,7 +39,30 @@ def cart_add(request):
 
 
 def cart_change(request):
-    ...
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html",
+        {"carts": cart},
+        request=request
+    )
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
@@ -58,7 +81,7 @@ def cart_remove(request):
     )
 
     response_data = {
-        "message": "Товар добавлен в корзину",
+        "message": "Товар удален из корзины",
         "cart_items_html": cart_items_html,
         "quantity_deleted": quantity,
     }
