43093b84ab0b49c51e3f5efff19af2e75d222b70
KhomenokNastya <khomenok.nastya@bk.ru>
2024-02-18 17:34:39 +0300

    Custom validator of phone number

--- orders/forms.py
+++ orders/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 
@@ -19,3 +21,15 @@ class CreateOrderForm(forms.Form):
             ("1", 'True'),
         ]
     )
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
