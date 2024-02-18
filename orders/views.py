46c6e9578e9a037c5b9b551b86d35aef8d5e7252
KhomenokNastya <khomenok.nastya@bk.ru>
2024-02-18 18:03:00 +0300

    Final edit

--- orders/views.py
+++ orders/views.py
@@ -3,11 +3,13 @@ from django.db import transaction
 from django.forms import ValidationError
 from django.shortcuts import redirect, render
 from carts.models import Cart
+from django.contrib.auth.decorators import login_required
 
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
 
+@login_required
 def create_order(request):
     if request.method == 'POST':
         form = CreateOrderForm(data=request.POST)
@@ -66,6 +68,7 @@ def create_order(request):
     context = {
         'title': 'Home - Оформление заказа',
         'form': form,
+        'order': True,
     }
     return render(request, 'orders/create_order.html', context=context)
 
--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.db.models import Prefetch
 
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -78,9 +80,21 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
     context = {
         'title': 'Home - Личный кабинет',
-        'form': form
+        'form': form,
+        'orders': orders
     }
     return render(request, 'users/profile.html', context)
 
